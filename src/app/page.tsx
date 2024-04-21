77ca37e8e6c05892bbb538541debfc638c3b7327
Praise <anosikepraise@gmail.com>
2024-04-21 23:31:50 +0100

    call to action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,8 +22,8 @@ export default function Home() {
       <BestPortfolio/>
       <Testimonial/>
       <BlogNews/>
-      {/* <CallToAction/> */}
-      {/* <Footer/> */}
+      <CallToAction/>
+      <Footer/>
     </div>
   )
 }
